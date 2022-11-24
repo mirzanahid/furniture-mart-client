312bfe964b8e1d5dc5d9b82dfcd84f9369d584bd
mirzanahid <mirzasaikat3@gmail.com>
2022-11-24 17:30:11 +0600

    banner added with responsive

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import banner1 from '../../../assets/banner01.jpg';
-import banner2 from '../../../assets/banner02.jpg';
-import banner3 from '../../../assets/banner03.jpg';
+import { Link } from 'react-router-dom';
+import banner1 from '../../../assets/banner1.jpg';
+import banner2 from '../../../assets/banner2.jpg';
+import banner3 from '../../../assets/banner3.jpg';
+
 import './Banner.css';
 const Banner = () => {
     return (
-
+        <Carousel>
+            <Carousel.Item interval={6000}>
+                <img
+                    className="d-block w-100 h-100 slider-image"
+                    src={banner1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3 className='banner-heading'>Get Low prices and best quality furniture</h3>
+                    <p className='banner-pra'>Used furniture is the great resource which is low price but in best quality</p>
+                    <Link className='regular-btn'>Shop Now</Link>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={6000}>
+                <img
+                    className="d-block w-100 h-100 slider-image"
+                    src={banner2}
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                    <h3 className='banner-heading'>Renovate your home</h3>
+                    <p className='banner-pra'>Buy used furniture at a cheaper rate!</p>
+                    <Link className='regular-btn'>Shop Now</Link>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 h-100 slider-image"
+                    src={banner3}
+                    alt="Third slide"
+                />
+                <Carousel.Caption>
+                    <h3 className='banner-heading'>Your perfect room</h3>
+                    <p className='banner-pra'>Sells what you don't need and buy what you need.</p>
+                    <Link className='regular-btn'>Shop Now</Link>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
